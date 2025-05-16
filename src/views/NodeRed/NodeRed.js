e4eb51afa8e06de2a329bab0de12769085472c6b
Jakob Schanz <Jakob Schanz>
2025-05-16 10:24:45 +0100

    add new flow existing check

--- src/views/NodeRed/NodeRed.js
+++ src/views/NodeRed/NodeRed.js
@@ -17,122 +17,123 @@ import styles from "assets/jss/material-dashboard-react/views/nodeRedStyle.js";
 const useStyles = makeStyles(styles);
 
 export default function NodeRed() {
-  const flowNameRef = useRef();
-  const classes = useStyles();
+    const flowNameRef = useRef();
+    const classes = useStyles();
 
-  const [flows, setFlows] = useState([]);
+    const [flows, setFlows] = useState([]);
 
- async function handleAddFlow() {
-  const flowName = flowNameRef.current.value;
-  if (flowName === "") {
-    alert("No name defined");
-    return;
-  }
+    async function getExistingFlowData (){
+        const res = await fetch("http://localhost:8000/flows");
+        const data = await res.json();
+        return data;
+    } 
 
-  const id = await hashFlowNameForIDs(flowName + Date.now());
-  console.log("Generated ID:", id);
+    async function handleAddFlow() {
+        const data = await getExistingFlowData();
+        const flowName = flowNameRef.current.value;
 
-  const newFlow = (
-    <GridItem xs="auto" sm="auto" md="auto" lg="auto" key={id}>
-      <Card>
-        <CardBody>
-          <div>
-            <Button
-              variant="contained"
-              color="primary"
-              style={{ marginRight: "5px" }}
-              onClick={() => alert("Open Flow")}
-            >
-              {flowName}
-            </Button>
-            <Button
-              variant="contained"
-              color="secondary"
-              onClick={() => alert("Settings")}
-            >
-              <SettingsIcon />
-            </Button>
-          </div>
-        </CardBody>
-      </Card>
-    </GridItem>
-  );
+        const existigFlowNames =[];
 
-  setFlows((prev) => [...prev, newFlow]);
-}
+        for (const flow of data) {
+            if (flow.type === "tab") {
+                existigFlowNames.push(flow.label)
+            }
+        }
 
+        if (flowName === "" || existigFlowNames.includes(flowName)) {
+            alert("Name incorect");
+            return;
+        }
 
-  function hashFlowNameForIDs(str, salt) {
-    let hash = 0;
-    const input = str + salt;
-    for (let i = 0; i < input.length; i++) {
-      hash = (hash << 5) - hash + input.charCodeAt(i);
-      hash |= 0;
+        const newFlow = (
+            <GridItem xs="auto" sm="auto" md="auto" lg="auto" key={flowName}>
+                <Card>
+                    <CardBody>
+                        <div>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                style={{ marginRight: "5px" }}
+                                onClick={() => alert("Open Flow")}
+                            >
+                                {flowName}
+                            </Button>
+                            <Button
+                                variant="contained"
+                                color="secondary"
+                                onClick={() => alert("Settings")}
+                            >
+                                <SettingsIcon />
+                            </Button>
+                        </div>
+                    </CardBody>
+                </Card>
+            </GridItem>
+        );
+        setFlows((prev) => [...prev, newFlow]);
     }
-    return hash >>> 0;
-  }
 
+    return (
+        <div>
+            <GridContainer>
+                <GridItem xs={12} sm={12} md={12}>
+                    <Card plain>
+                        <CardHeader plain color="primary">
+                            <h4 className={classes.cardTitleWhite}>Node-Red</h4>
+                            <p className={classes.cardCategoryWhite}>
+                                Flows for the visualisation of diagrams
+                            </p>
+                        </CardHeader>
 
-  return (
-    <div>
-      <GridContainer>
-            <GridItem xs={12} sm={12} md={12}>
-                <Card plain>
-                    <CardHeader plain color="primary">
-                        <h4 className={classes.cardTitleWhite}>Node-Red</h4>
-                        <p className={classes.cardCategoryWhite}>
-                            Flows for the visualisation of diagrams
-                        </p>
-                    </CardHeader>
+                        <GridContainer>
+                            <GridItem xs={12} sm={12} md={12}>
+                                <Card>
+                                    <CardHeader plain color="primary" className={classes.smallCardHeader}>
+                                        <h4 className={classes.cardTitleBlack}>Settings</h4>
+                                    </CardHeader>
 
-                    <GridContainer>
-                        <GridItem xs={12} sm={12} md={12}>
-                            <Card>
-                                <CardHeader plain color="primary" className={classes.smallCardHeader}>
-                                    <h4 className={classes.cardTitleBlack}>Settings</h4>
-                                </CardHeader>
-                                <CardBody>
-                                    <div id="node-red-settings" style={{ display: "flex", alignItems: "center" }}>
-                                        <TextField
-                                            inputRef={flowNameRef}
-                                            label="Flow Name"
-                                            variant="outlined"
-                                            size="small"
-                                            margin="normal"
-                                            InputProps={{
-                                                style: {
+                                    <CardBody>
+                                        <div id="node-red-settings" style={{ display: "flex", alignItems: "center" }}>
+                                            <TextField
+                                                inputRef={flowNameRef}
+                                                label="Flow Name"
+                                                variant="outlined"
+                                                size="small"
+                                                margin="normal"
+                                                InputProps={{
+                                                    style: {
                                                     height: "40px", 
                                                     flex: 1, 
                                                 },
-                                            }}
-                                            style={{
-                                                marginRight: "10px", 
-                                            }}
-                                        />
+                                                }}
+                                                style={{
+                                                    marginRight: "10px", 
+                                                }}
+                                            />
 
-                                        <Button
-                                            id="add-new-flow"
-                                            variant="contained"
-                                            color="primary"
-                                            onClick={handleAddFlow}
-                                            style={{
-                                                height: "40px",
-                                            }}
-                                        >
-                                            Add Flow
-                                        </Button>
-                                    </div>
-                                </CardBody>
-                            </Card>
-                        </GridItem>
-                    </GridContainer>
+                                            <Button
+                                                id="add-new-flow"
+                                                variant="contained"
+                                                color="primary"
+                                                onClick={handleAddFlow}
+                                                style={{
+                                                    height: "40px",
+                                                }}
+                                            >
+                                                Add Flow
+                                            </Button>
+                                        </div>
+                                    </CardBody>
+                                </Card>
+                            </GridItem>
+                        </GridContainer>
 
-                    <GridContainer spacing={3} style={{ flexWrap: "wrap" }}>
-                        {flows.map((flow) => flow)}
-                    </GridContainer>
-                </Card>
-            </GridItem>
-      </GridContainer>
-    </div>
-  );
+                        <GridContainer spacing={3} style={{ flexWrap: "wrap" }}>
+                            {flows.map((flow) => flow)}
+                        </GridContainer>
+                    </Card>
+                </GridItem>
+            </GridContainer>
+        </div>
+    );
 }
