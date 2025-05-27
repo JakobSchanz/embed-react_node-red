174754ff90b7d5e3a79490b9566810c8ac062eef
Jakob Schanz <Jakob Schanz>
2025-05-27 10:40:17 +0100

    clean code

--- src/views/NodeRed/NodeRed.js
+++ src/views/NodeRed/NodeRed.js
@@ -2,20 +2,37 @@ import React from "react";
 
 import { Box } from "@material-ui/core";
 
+const config = {
+    style: {
+        box: {
+            displayFlex: "flex",
+            direktion: "column",
+            heightConf: "80vh",
+            oferflowHid: "hidden",
+        },
+        iFrame: {
+            size: "100%",
+            borderN: "none",
+        },
+    },
+    address: "http://localhost:8000",
+    title: "Node-RED",
+}
+
 export default function NodeRedView() {
   return (
-        <Box display="flex" flexDirection="column" height="80vh">
-            <Box flex={1} overflow="hidden">
+        <Box display={config.style.box.displayFlex} flexDirection={config.style.box.direktion} height={config.style.box.heightConf}>
+            <Box flex={1} overflow={config.style.box.oferflowHid}>
                 <iframe
-                    src="http://localhost:8000"
-                    title="Node-RED"
+                    src={config.address}
+                    title={config.title}
                     style={{
-                        width: "100%",
-                        height: "100%",
-                        border: "none",
+                        width: config.style.iFrame.size,
+                        height: config.style.iFrame.size,
+                        border: config.style.iFrame.borderN,
                     }}
                 />
             </Box>
         </Box>
     );
-}
+} 
